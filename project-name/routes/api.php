c617846280cfa16a6038c22cf9a4627192924566
Daniel Villare <villaredanielt05@gmail.com>
2024-06-09 20:22:08 +0800

    update(nakita ko na error sa code walang square bracket yung sa api)

--- project-name/routes/api.php
+++ project-name/routes/api.php
@@ -3,11 +3,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DoctorController;
 
-    Route::get('/doctors', DoctorController::class, 'index');
-    
-    Route::post('/doctors', DoctorController::class, 'store');
-    Route::get('/doctors/{id}', DoctorController::class, 'show');
-    Route::put('/doctors/{id}', DoctorController::class, 'update');
-    Route::delete('/doctors/{id}', DoctorController::class, 'destroy');
+    Route::get('/doctors', [DoctorController::class, 'index']);
+    Route::post('/doctors', [DoctorController::class, 'store']);
+    Route::get('/doctors/{id}', [DoctorController::class, 'show']);
+    Route::put('/doctors/{id}', [DoctorController::class, 'update']);
+    Route::delete('/doctors/{id}', [DoctorController::class, 'destroy']);
 
 ?>
